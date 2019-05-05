3e920f103b2a6729f2e1f89a267034cc6127ba18
‘yhishi’ <yhishi@example.com>
2019-05-05 22:24:34 +0900

    起動時にカレンダーを表示

---
+++ app/src/main/java/yoshikii/com/record_nutrients/view/CalendarFragment.kt
@@ -0,0 +1,49 @@
+package yoshikii.com.record_nutrients.view
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.widget.DatePicker
+import java.util.*
+
+
+class CalendarFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+    interface OnDateSelectedListener {
+        /** 選択した年月日を返す */
+        fun onSelected(year: Int, month: Int, day: Int)
+    }
+
+    private lateinit var listener: OnDateSelectedListener
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if (context is OnDateSelectedListener) {
+            listener = context
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val calendar = Calendar.getInstance()
+
+        return DatePickerDialog(
+            requireContext(),
+            this,
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, day: Int) {
+        // monthは(0-11)のため、1を加算
+        listener.onSelected(year, month + 1, day)
+    }
+
+    companion object {
+        fun newInstance(): CalendarFragment = CalendarFragment()
+        const val TAG_CALENDAR_FRAGMENT = "CalendarFragment"
+    }
+}
--- app/src/main/java/yoshikii/com/record_nutrients/view/MainActivity.kt
+++ app/src/main/java/yoshikii/com/record_nutrients/view/MainActivity.kt
@@ -5,17 +5,25 @@ import android.support.v7.app.AppCompatActivity
 import yoshikii.com.record_nutrients.R
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CalendarFragment.OnDateSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         if (savedInstanceState == null) {
-            supportFragmentManager
-                    .beginTransaction()
-                    .add(R.id.fragment_container, MealListFragment(), MealListFragment.TAG_MEAL_LIST_FRAGMENT)
-                    .commit()
+            /** [CalendarFragment]表示 */
+            CalendarFragment
+                .newInstance()
+                .show(supportFragmentManager, CalendarFragment.TAG_CALENDAR_FRAGMENT)
         }
+    }
+
+    /** [CalendarFragment]で日付選択した値を受け取る */
+    override fun onSelected(year: Int, month: Int, day: Int) {
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragment_container, MealListFragment(), MealListFragment.TAG_MEAL_LIST_FRAGMENT)
+            .commit()
     }
 }
